1479f05e73691555efd266ba4929fad3208770fd
karel <karelpuerto78@gmail.com>
2020-06-27 19:10:00 -0500

    e-comerce end

--- database/seeds/InitiatorSeeder.php
+++ database/seeds/InitiatorSeeder.php
@@ -15,9 +15,20 @@ class InitiatorSeeder extends Seeder
         // Create admin user
         factory(User::class)->create([
             'name' => 'admin',
-            'email' => 'admin@ecommer.com',
+            'email' => 'admin@ecommerce.com',
         ]);
         // Populate the user table with some data
         factory(User::class, 10)->create();
+
+        \Laravel\Passport\Client::query()->create([
+           'id' => 2,
+           'name' => 'ecommerce',
+           'secret' => 'LLxazzgHNfxkNv7xmdxOcfIZSmsg4EQjJ1BzJwpU',
+           'provider' => 'users',
+           'redirect' => '',
+           'personal_access_client' => 0,
+           'password_client' => 1,
+           'revoked' => 0
+        ]);
     }
 }
