2dbbd03821b338a591b4546579b32d19e4aba084
karel <karelpuerto78@gmail.com>
2020-06-28 10:00:34 -0500

    e-comerce end

---
+++ public/js/chunk-2d0c9766.0ae8aa9d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c9766"],{"58f3":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-col",{attrs:{col:"12"}},[t("h1",[n._v("Coming Soon :)")])])},o=[],a={name:"Clients"},l=a,s=t("2877"),r=t("6544"),u=t.n(r),i=t("62ad"),p=Object(s["a"])(l,c,o,!1,null,"44c3b7a4",null);e["default"]=p.exports;u()(p,{VCol:i["a"]})}}]);
+//# sourceMappingURL=chunk-2d0c9766.0ae8aa9d.js.map
